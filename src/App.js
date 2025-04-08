a1dac87a925c8430331078fad40f4001162b2c21
AyushKumarSingh <ayushkumar7355488975@gmail.com>
2025-04-08 18:49:33 +0530

    updated structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Example components for different pages
+import Home from './components/Home'; 
+import About from './components/About'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>Welcome to the ERP Clone</p>
+          {/* Navigation Links */}
+          <nav>
+            <a href="/" className="App-link">Home</a>
+            <a href="/about" className="App-link">About</a>
+          </nav>
+
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
